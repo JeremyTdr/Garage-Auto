ce44e4cc724f188d1eea0acbf10eb79e9e6c4e13
Jeremy Tdr <jeremy.tdr@gmail.com>
2023-07-10 17:29:53 +0200

    Navbar v1

--- pages/base.php
+++ pages/base.php
@@ -1,5 +1,8 @@
 <?php include 'pages/includes/html-head.php'; ?>
 
 <body>
+
+    <?php include 'pages/includes/navbar.php'; ?>
+
     <!-- Début de div Contents -->
     <div class="contents">
---
+++ pages/includes/navbar.php
@@ -0,0 +1,14 @@
+<nav class="nav">
+
+    <img class="logo" src="src/assets/logo-nobg.png" alt="logo du garage"></img>    
+
+    <ul class="nav-items" id="nav-items">
+        <li class="nav-item">Accueil</li>
+        <li class="nav-item">Nos services</li>
+        <li class="nav-item">Les occasions</li>
+        <li class="nav-item reserved">Nous contacter</li>
+    </ul>
+
+    <i class="fas fa-bars burger-btn" id="burger-btn"></i>
+    
+</nav>
