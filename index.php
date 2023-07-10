9e7ec55d8effeb3bd0a90b8721e989b0250deb3b
Jeremy Tdr <jeremy.tdr@gmail.com>
2023-07-10 16:51:47 +0200

    PHP, SASS, Style base init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include './pages/base.php'; ?>
+
+<div class="section accueil" id="accueil">
+    <div class="content">
+    <h2>Accueil</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus molestias necessitatibus alias corporis modi itaque magni beatae facilis voluptate</p>
+    </div>
+</div>
+
+<?php include './pages/includes/html-foot.php'; ?>
---
+++ pages/base.php
@@ -0,0 +1,5 @@
+<?php include 'pages/includes/html-head.php'; ?>
+
+<body>
+    <!-- Début de div Contents -->
+    <div class="contents">
