280e13611e4964aee8810ddea5f490a495c88064
Jeremy Tdr <jeremy.tdr@gmail.com>
2023-07-19 16:01:02 +0200

    Header mobile/desktop v1

---
+++ pages/sections/header.php
@@ -0,0 +1,10 @@
+<section class="header" id="header">
+    <div class="background-img"></div>
+    <div class="header-content">
+        <h1>Garage Auto <br> V. Parrot</h1>
+        <div class="header-btns">
+            <div class="header-btn services">Nos services</div>
+            <div class="header-btn autos">Notre parc auto d'occasion</div>
+        </div>
+    </div>
+</section>
