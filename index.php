0625e737ba169e058542df39e341cbd4f0fbf86f
Jeremy Tdr <jeremy.tdr@gmail.com>
2023-07-25 18:43:30 +0200

    Resume section V1 + Navbar update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include './pages/base.php'; ?>
 
 <?php include './pages/sections/header.php'; ?>
+<?php include './pages/sections/resume.php'; ?>
 
 <?php include './pages/includes/html-foot.php'; ?>
