d597b65d5a4d1d6877be429fdaffa222a8487791
Jeremy Tdr <jeremy.tdr@gmail.com>
2024-02-14 15:38:53 +0100

    update heroku

--- admin.php
+++ admin.php
@@ -10,7 +10,9 @@ include 'pages/includes/html-head.php';
         <div class="admin-section admin-console">
             
             <div class="admin-title">Centre d'administration</div>
-            
+            <p class="admin-text">Bienvenue dans le centre d'administration du Garage Parrot.</p>
+            <br>
+            <p class="admin-text">Ici, vous pouvez naviguer dans vos différents onglets pour administrer le site web du garage.</p>
                 
         </div>
     
