d02dde0026bac943079c88bb7accd1f7fc47a92f
Jeremy Tdr <jeremy.tdr@gmail.com>
2024-02-14 16:03:03 +0100

    JawsDB add to database file

--- actions/database.php
+++ actions/database.php
@@ -1,7 +1,25 @@
 <?php
 
+//Heroku - JawsDB
+if (getenv('JAWSDB_URL') !== false) {
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'], './');
+
+} else {
+    // LOCAL
+    $hostname = 'localhost:8889';
+    $username = 'root';
+    $password = 'root';
+    $database = 'garage_auto';
+}
+
 try {
-    $bdd = new PDO('mysql:host=localhost:8889;dbname=garage_auto', 'root', 'root');
+    $bdd = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
