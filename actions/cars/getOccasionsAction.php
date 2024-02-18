bfe440875cc7a4ac34a2694a427820f311323c7c
Jeremy Tdr <jeremy.tdr@gmail.com>
2024-02-18 17:09:51 +0100

    Occasion filter added

--- actions/cars/getOccasionsAction.php
+++ actions/cars/getOccasionsAction.php
@@ -3,3 +3,23 @@ require('actions/database.php');
 
 //Récupération de la liste des offres d'occasion
 $getOccasions = $bdd->query('SELECT * FROM offers ORDER BY id');
+
+
+//Filtrage de la liste des offres d'occasion
+
+if(isset($_GET['yearMin']) || isset($_GET['yearMax']) || isset($_GET['priceMin']) || isset($_GET['priceMax']) || isset($_GET['kmsMin']) || isset($_GET['kmsMax'])) {
+
+    $yearMin = $_GET['yearMin'];
+    $yearMax = $_GET['yearMax'];
+    $priceMin = $_GET['priceMin'];
+    $priceMax = $_GET['priceMax'];
+    $kmsMin = $_GET['kmsMin'];
+    $kmsMax = $_GET['kmsMax'];
+
+    $getOccasions = $bdd->query("SELECT * FROM offers WHERE 
+                                year BETWEEN $yearMin AND $yearMax
+                                AND 
+                                price BETWEEN $priceMin AND $priceMax
+                                AND 
+                                kms BETWEEN $kmsMin AND $kmsMax");
+}
--- autos-occasion.php
+++ autos-occasion.php
@@ -11,29 +11,33 @@ include 'pages/includes/html-head.php';
                     <h2 class="page-title">Liste des occasions</h2>
 
                     <div class="filter-section">
-                        <form class="filter-content" method="POST">
+                        <form class="filter-content" method="GET">
                             <div class="filters">
                                 <div class="filter">
                                     <label for="yearFilter" class="modal-label">Année :</label>
-                                    <input type="text" class="modal-input form_data" name="yearMin" placeholder="Min">
+                                    <input type="text" name="yearMin" placeholder="Min" value="<?php if(isset($_GET['yearMin'])){echo $_GET['yearMin'];} else {echo '0';}?>">
                                     <span>-</span>
-                                    <input type="text" class="modal-input form_data" name="yearMax" placeholder="Max">
+                                    <input type="text" name="yearMax" placeholder="Max" value="<?php if(isset($_GET['yearMax'])){echo $_GET['yearMax'];} else {echo '2024';}?>">
                                 </div>
                                 <div class="filter">
                                     <label for="priceFilter" class="modal-label">Prix :</label>
-                                    <input type="text" class="modal-input form_data" name="priceMin" placeholder="Min">
+                                    <input type="text" name="priceMin" placeholder="Min" value="<?php if(isset($_GET['priceMin'])){echo $_GET['priceMin'];} else {echo '0';}?>">
                                     <span>-</span>
-                                    <input type="text" class="modal-input form_data" name="priceMax" placeholder="Max">
+                                    <input type="text" name="priceMax" placeholder="Max" value="<?php if(isset($_GET['priceMax'])){echo $_GET['priceMax'];} else {echo '90000';}?>">
                                 </div>
                                 <div class="filter">
                                     <label for="kmsFilter" class="modal-label">Kilométrage :</label>
-                                    <input type="text" class="modal-input form_data" name="kmsMin" placeholder="Min">
+                                    <input type="text" name="kmsMin" placeholder="Min" value="<?php if(isset($_GET['kmsMin'])){echo $_GET['kmsMin'];} else {echo '0';}?>">
                                     <span>-</span>
-                                    <input type="text" class="modal-input form_data" name="kmsMax" placeholder="Max">
+                                    <input type="text" name="kmsMax" placeholder="Max" value="<?php if(isset($_GET['kmsMax'])){echo $_GET['kmsMax'];} else {echo '300000';}?>">
                                 </div>
                             </div>
                             
-                            <button type="button" class="filter-btn"><i class="fas fa-plus"></i> Filtrer</button>
+                            <div class="filter-btns">
+                                <button type="submit" class="filter-btn"><i class="fas fa-plus"></i> Filtrer</button>
+                                <a class="filter-btn"><i class="fas fa-minus"></i> Supprimer les filtres</a>
+                            </div>
+                            
                         </form>   
                     </div>
 
